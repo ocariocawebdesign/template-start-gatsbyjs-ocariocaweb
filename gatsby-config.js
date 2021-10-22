4e3d02678f5619abb0ea47b948081558af92f8d6
Carlos <ocariocawebdesign@gmail.com>
2021-10-22 08:23:25 -0300

    Novas correções, update e modificações no template

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,47 +5,44 @@
  */
 module.exports = {
   siteMetadata: {
-    title: 'Site Gatsby O Carioca',
-    siteUrl: 'https://www.ocariocaweb.com',
-    description: 'Site de desenvolvimento Carioca Web',
+    title: "Site Gatsby O Carioca",
+    siteUrl: "https://www.ocariocaweb.com",
+    description: "Site de desenvolvimento Carioca Web",
 
-    social: [{
-        name: 'github',
-        url: 'https://github.com/ocariocawebdesign',
+    social: [
+      {
+        name: "github",
+        url: "https://github.com/ocariocawebdesign",
       },
       {
-        nome: 'linkedin',
-        url: 'https://www.linkedin.com/in/ocariocawebdesign/',
+        nome: "linkedin",
+        url: "https://www.linkedin.com/in/ocariocawebdesign/",
       },
-
     ],
-
   },
   plugins: [
-
     `gatsby-plugin-react-helmet`,
 
     `gatsby-plugin-sitemap`,
 
-    /*{
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name:'O Carioca Web',
-        short_name:'O Carioca Web',
-        background_color:'#fff',
-        theme_color:`#440055`,
+        name: "O Carioca Web",
+        short_name: "O Carioca Web",
+        background_color: "#fff",
+        theme_color: `#440055`,
         // Setting a color is optional.
-       display:'standalone',
-       icon:'src/img/monster.png',
+        display: "standalone",
+        icon: "src/img/monster.png",
         // Disable the loading spinner.
         showSpinner: false,
       },
     },
-*/
+
     //-------------
 
     //-----------
-
 
     {
       resolve: `gatsby-plugin-nprogress`,
@@ -54,23 +51,19 @@ module.exports = {
         color: `tomato`,
         // Disable the loading spinner.
         showSpinner: false,
-
       },
     },
-
 
     //-----------
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
-
-        //displayName: process.env.NODE_ENV == 'production' ? true : false, 
+        //displayName: process.env.NODE_ENV == 'production' ? true : false,
       },
     },
 
     //-----------
-
 
     {
       resolve: `gatsby-plugin-canonical-urls`,
@@ -83,25 +76,18 @@ module.exports = {
 
     //-----------
 
-
-
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-
       },
     },
 
     /*-----------------------------------*/
 
-
-
     /*-----------------------------*/
 
-    // `gatsby-plugin-offline`,
-
-
-  ]
+    `gatsby-plugin-offline`,
+  ],
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,26 @@ import React from "react"
 
 const Footer = () => (
   <footer>
-    <h3 className="text-center">Footer</h3>
+    <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+              <div id="box-footer-1">
+                  <p className="text-center">Template Starter Gatsbyjs - Desenvolvido por Carlos Abreu - O Carioca Web </p>
+              </div>
+        </div>
+      <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+              <div id="box-footer-2">
+              <h3 className="text-center"> </h3>
+              <span>Icone github</span> 
+              <span>Icone Instagram</span>                    
+              <span>Ícone Whatsappp</span>                    
+                   
+                    
+                 
+              </div>
+      </div>
+    </div>
+    </div>
   </footer>
 )
 
